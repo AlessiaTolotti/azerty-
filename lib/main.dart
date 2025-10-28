74a6fe5e2bad9df015c03015c1aa9745bdfc56b0
AlessiaTolotti <alessia.tolotti.24@stud.itsaltoadriatico.it>
2025-10-28 17:28:55 +0100

    fix es1.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ColorChangerPage(),
+    );
+  }
+}
+
+
 class ColorChangerPage extends StatefulWidget {
   const ColorChangerPage({super.key});
   @override
